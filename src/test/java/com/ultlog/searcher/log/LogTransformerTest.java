c38f0a7e02f086bc0662a1289322d019cfc3bf00
will <64425242+willser@users.noreply.github.com>
2020-08-14 20:39:39 +0800

    test: change ut

--- src/test/java/com/ultlog/searcher/log/LogTransformerTest.java
+++ src/test/java/com/ultlog/searcher/log/LogTransformerTest.java
@@ -16,19 +16,6 @@ public class LogTransformerTest {
     @Autowired
     private LogTransformer logTransformer;
 
-    String logString = "2020-07-21 09:16:37 [ERROR] [pool-420-thread-7] c.t.g.h.HttpConnectionPoolUtil - test error \n" +
-            "java.lang.RuntimeException: tea das asdsawda asdas a\n" +
-            "at com.example.demo.DemoApplicationTests.contextLoads(DemoApplicationTests.java:21)\n" +
-            "at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\n" +
-            "at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\n" +
-            "at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\n" +
-            "at java.lang.reflect.Method.invoke(Method.java:498)\n" +
-            "at org.junit.platform.commons.util.ReflectionUtils.invokeMethod(ReflectionUtils.java:675)\n";
-
-
-    String pattern = " <pattern>%d{yyyy-MM-dd HH:mm:ss} [%-5level] [%-5thread] %logger{20} - %msg%n</pattern>";
-
-
     @Test
     public void testReadLogFromString() {
         String d = "2020-07-21 09:16:37 [ERROR] [pool-420-thread-7] c.t.g.h.HttpConnectionPoolUtil - test error \n" +
